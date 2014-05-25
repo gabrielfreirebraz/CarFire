a3bef6b8ed45f74daac64aeef9337aeaee2310d5
Gabriel Freire <gabrielfreire14@hotmail.com>
2014-05-25 20:59:56 -0300

    melhoria no js dataTable

--- WebContent/resources/js/jquery.dataTables.js
+++ WebContent/resources/js/jquery.dataTables.js
@@ -1,5 +1,5 @@
 /*! DataTables 1.10.0
- * ©2008-2014 SpryMedia Ltd - datatables.net/license
+ * Â©2008-2014 SpryMedia Ltd - datatables.net/license
  */
 
 /**
@@ -112,7 +112,7 @@
 	
 	// U+2009 is thin space and U+202F is narrow no-break space, both used in many
 	// standards as thousands separators
-	var _re_formatted_numeric = /[',$£€¥%\u2009\u202F]/g;
+	var _re_formatted_numeric = /[',$Â£â‚¬Â¥%\u2009\u202F]/g;
 	
 	
 	var _empty = function ( d ) {
